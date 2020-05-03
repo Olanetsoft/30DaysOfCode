0827bf535e07bd1ced8b8c8bf4ae71fc78c43fec
olanetsoft <heedris2olubisi@gmail.com>
2020-05-03 12:21:51 +0100

    PUSHED CODE FOR DAY 3

---
+++ Day3/Task3.js
@@ -0,0 +1,18 @@
+//the function is created below
+function everythingIsPackaging(insertTheNumber, theWidth) {
+
+    //convert the second argument to string and then minus the length from the first argument
+    insertTheNumber -= theWidth.toString().length;
+
+
+    //check if the number is greater than zero i.e a positive number
+    if (insertTheNumber > 0) {
+
+        //Return a new array with the first argument and test result of the second argument
+        return new Array(insertTheNumber + (/\./.test(theWidth) ? 2 : 1)).join('0') + theWidth;
+    }
+
+    // This will always return a string
+    return theWidth + ""; 
+}
+everythingIsPackaging(5, 22);
