31eea9b627f907af41c3fec60e3827c9d8124469
olanetsoft <heedris2olubisi@gmail.com>
2020-05-14 22:20:06 +0100

    completed

---
+++ Day14/Task14.js
@@ -0,0 +1,11 @@
+// Function to arithmetically find bee number
+function beeNumber (n){
+
+    //Declare the index for starting point
+    var index = 1
+
+    //return the multiple minus index
+    return n * 2 - index; 
+}  
+
+beeNumber(1);
